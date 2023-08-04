4417ea623cde61de875392953d55ca8d33e4e451
Jenny <isjia@163.com>
2023-08-05 02:20:03 +0800

    init commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1'
+  })
+)
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+
+server.use(router)
+
+const port = 3333
+server.listen(port, () => {
+  console.log('JSON Server is running on port: ', port)
+})
